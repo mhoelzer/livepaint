29ff17132dbf2b96311523db32e7f3e3058d9f39
Michelle Hoelzer <hoelzermm@gmail.com>
2018-10-23 15:08:37 -0400

    some initial changes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 
+let updates = [];
 // Fill in your request handlers here
+app.post("/updates", (request, response) => {
+    
+})
 
 app.listen(port)
--- public/polling.js
+++ public/polling.js
@@ -1,1 +1,6 @@
 // Add logic to this script to poll server every second for updated pixels.
+let clientUpdates = 0;
+
+function pollUpdatedPixels() {
+    
+}
